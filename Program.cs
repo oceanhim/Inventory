4f5fe2958732c026776859e2104681e03642ff8c
oceanhi <ocean@chunkychode.com>
2023-04-02 18:03:12 -0700

    Fixed Issue #1

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace app
         public static void Main(string[] args)
         {
             Entity Player = new Entity("Ocean",100.0f,100.0f);
-            Item apple = (Item)Collectibles.GetItem("Apple");
+            Consumable apple = (Consumable)Collectibles.GetItem("Apple");
             Entity p2 = Player.Clone();
             Item a2 = apple.Clone(); // Not Working
             Console.WriteLine();
